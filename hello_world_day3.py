4d792b6f079062017f9c2e86eb43d5f4f9bfb88a
MichaelCarter999 <12.michael.carter@gmail.com>
2026-04-29 10:44:43 +0100

    Update hello_world_day3.py

--- hello_world_day3.py
+++ hello_world_day3.py
@@ -3,4 +3,4 @@ from extras.scripts import Script
 class HelloJobs(Script):
 
     def run(self, data, commit):
-        self.log_debug("Hello, this is my first Nautobot Job.")
+        self.log_info("Hello, this is my first Nautobot Job.")
