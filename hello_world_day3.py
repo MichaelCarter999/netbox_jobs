955ba0bb36832ade2c2719e264affcd80567009b
MichaelCarter999 <12.michael.carter@gmail.com>
2026-04-29 11:45:35 +0100

    Update hello_world_day3.py

--- hello_world_day3.py
+++ hello_world_day3.py
@@ -1,6 +1,12 @@
 from extras.scripts import Script
 
+name = "Hello World Netbox Jobs"
+
 class HelloJobs(Script):
 
+    class Meta:
+        name = "Hello Jobs"
+        description = "Hello World for first Netbox Job"
+        
     def run(self, data, commit):
-        self.log_info("Hello, this is my first Nautobot Job.")
+        self.log_info("Hello, this is my first Netbox Job.")
