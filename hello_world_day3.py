86a51df699a300b19a72a95f3bb2c8077c65716c
MichaelCarter999 <12.michael.carter@gmail.com>
2026-04-29 12:12:14 +0100

    Update hello_world_day3.py

--- hello_world_day3.py
+++ hello_world_day3.py
@@ -7,8 +7,16 @@ class HelloJobs(Script):
         description = "Hello World for first Netbox Job"
         
     def run(self, data, commit):
+        self.log_info("Hello, this is my first Netbox Job.")
+
+class HelloLoggingJobs(Script):
+
+    class Meta:
+        name = "Hello Logging Jobs"
+        description = "Hello World for first Netbox Logging Job"
+        
+    def run(self, data, commit):
         self.log_info("This is an info type log.")
-        self.log_debug("DEBUG: This is a debug type log.")
+        self.log_debug("This is a debug type log.")
         self.log_warning("This is a warning type log.")
-        self.log_failure("This is an error type log.")
-        self.log_failure("This is a critical type log.")
+        self.log_failure("This is a failure type log.")
