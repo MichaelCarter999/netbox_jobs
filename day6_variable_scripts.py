85bc5bf242c25941fb6e7add00149a9cbb5f39b5
MichaelCarter999 <12.michael.carter@gmail.com>
2026-04-30 12:02:57 +0100

    Create day6_variable_scripts.py

---
+++ day6_variable_scripts.py
@@ -0,0 +1,23 @@
+from extras.scripts import Script, StringVar, IntegerVar
+
+class HelloVariables(Script):
+
+    message = StringVar(
+        description="Message to display"
+    )
+
+    days = IntegerVar(
+        description="Number of days",
+        default=10
+    )
+
+    class Meta:
+        name = "Hello Variables"
+        description = "Scripts Variable Examples"
+        ordering = "Day 6 Variables"
+
+    def run(self, data, commit):
+        message = data["message"]
+        days = data["days"]
+
+        self.log_debug(f"Please give the message: {message} in {days} days.")
