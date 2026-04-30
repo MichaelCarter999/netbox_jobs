cfdd13c4a139df6c528b43afd1db9e506937ed47
MichaelCarter999 <12.michael.carter@gmail.com>
2026-04-30 15:07:58 +0100

    Update day6_variable_scripts.py

--- day6_variable_scripts.py
+++ day6_variable_scripts.py
@@ -1,4 +1,4 @@
-from extras.scripts import Script, StringVar, IntegerVar, MultiSelectVar
+from extras.scripts import Script, StringVar, IntegerVar
 
 class HelloVariables(Script):
 
@@ -11,18 +11,6 @@ class HelloVariables(Script):
         default=10
     )
 
-
-    CHOICES = (
-        ('h', 'Happy'),
-        ('s', 'Sad'),
-        ('e', 'Excited'),
-    )    
-    
-    feelings = MultiSelectVar(
-        choices=CHOICES,
-        description="How are you feeling?"
-    )
-    
     class Meta:
         name = "Hello Variables"
         description = "Scripts Variable Examples"
@@ -30,7 +18,5 @@ class HelloVariables(Script):
     def run(self, data, commit):
         message = data["message"]
         days = data["days"]
-        feelings = data["feelings"]
         
         self.log_info(f"Please give the message: {message} in {days} days.")
-        self.log_info(f"I am feeling {feelings}!")
