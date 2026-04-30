1990eb838e28c5ad34a195df7240dda42c0af626
MichaelCarter999 <12.michael.carter@gmail.com>
2026-04-30 15:29:19 +0100

    Update multiselecttest.py

--- multiselecttest.py
+++ multiselecttest.py
@@ -1,22 +1,24 @@
-from extras.scripts import Script, MultiSelectVar
-
-CHOICES = (
-    ("h", "Happy"),
-    ("s", "Sad"),
-    ("e", "Excited"),
-)
+from extras.scripts import Script, MultiChoiceVar
 
 class HelloVariables(Script):
 
-    feelings = MultiSelectVar(
+    CHOICES = (
+        ('h', 'Happy'),
+        ('s', 'Sad'),
+        ('e', 'Excited'),
+        ('b', 'Bored'),
+    )    
+    
+    feelings = MultiChoiceVar(
         choices=CHOICES,
         description="How are you feeling?"
     )
-
+    
     class Meta:
         name = "Hello Variables"
         description = "Scripts Variable Examples"
 
     def run(self, data, commit):
         feelings = data["feelings"]
+        
         self.log_info(f"I am feeling {feelings}!")
